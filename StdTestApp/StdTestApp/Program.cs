0308f06be327c66c90ee5c164663b0b0f8f23d62
Datuone <145194303+Datuone@users.noreply.github.com>
2023-09-16 12:45:34 +0300

    Added HelloWorld

--- StdTestApp/StdTestApp/Program.cs
+++ StdTestApp/StdTestApp/Program.cs
@@ -10,6 +10,8 @@ namespace StdTestApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
